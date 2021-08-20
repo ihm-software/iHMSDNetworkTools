ca7f68a052e8c248efbbaa0230c76348d203f64d
Jason Diaz <35275171+JasonDTX@users.noreply.github.com>
2021-08-20 14:07:54 -0500

    Code Signing certificate, fixed math in speedtest

--- NetworkInformation_rc_0.0.1.ps1
+++ NetworkInformation_rc_0.0.1.ps1
@@ -1,4 +1,4 @@
-﻿Function Start-Log {
+Function Start-Log {
     <#
   .SYNOPSIS
     Creates log file
@@ -329,11 +329,287 @@ Function Send-Log {
         }
     }
 }
+function Get-NetworkInformation {
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    [OutputType('System.Object[]')]
+    param (
+        [Parameter(Mandatory=$False)][ValidateSet("Up","Down")]
+        [string]$InterfaceStatus,
+        [Parameter(Mandatory=$False)][ValidateSet("IPv4","IPv6")]
+        [string]$AddressFamily
+    )
+    begin{
+        if ($AddressFamily) {
+            if ($AddressFamily -eq "IPv4") {
+                $AddrFam = "InterNetwork"
+            }
+            if ($AddressFamily -eq "IPv6") {
+                $AddrFam = "InterNetworkV6"
+            }
+        }
+    }
+    process {
+        [System.Collections.Arraylist]$PSObjectCollection = @()
+        $interfaces = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
+        $InterfacesToExplore = $interfaces
+        if ($InterfaceStatus) {
+            $InterfacesToExplore = $InterfacesToExplore | Where-Object {$_.OperationalStatus -eq $InterfaceStatus}
+        }
+        if ($AddressFamily) {
+            $InterfacesToExplore = $InterfacesToExplore | Where-Object {$($_.GetIPProperties().UnicastAddresses | ForEach-Object {$_.Address.AddressFamily}) -contains $AddrFam}
+        }
+        foreach ($adapter in $InterfacesToExplore) {
+            $ipprops = $adapter.GetIPProperties()
+            $ippropsPropertyNames = $($ipprops | Get-Member -MemberType Property).Name
+            if ($AddressFamily) {
+                $UnicastAddressesToExplore = $ipprops.UnicastAddresses | Where-Object {$_.Address.AddressFamily -eq $AddrFam}
+            }
+            else {
+                $UnicastAddressesToExplore = $ipprops.UnicastAddresses
+            }
+            foreach ($ip in $UnicastAddressesToExplore) {
+                $FinalPSObject = [pscustomobject]@{}
+                $adapterPropertyNames = $($adapter | Get-Member -MemberType Property).Name
+                foreach ($adapterPropName in $adapterPropertyNames) {
+                    $FinalPSObjectMemberCheck = $($FinalPSObject | Get-Member -MemberType NoteProperty).Name
+                    if ($FinalPSObjectMemberCheck -notcontains $adapterPropName) {
+                        $FinalPSObject | Add-Member -MemberType NoteProperty -Name $adapterPropName -Value $($adapter.$adapterPropName)
+                    }
+                }
+                foreach ($ippropsPropName in $ippropsPropertyNames) {
+                    $FinalPSObjectMemberCheck = $($FinalPSObject | Get-Member -MemberType NoteProperty).Name
+                    if ($FinalPSObjectMemberCheck -notcontains $ippropsPropName -and
+                        $ippropsPropName -ne "UnicastAddresses" -and $ippropsPropName -ne "MulticastAddresses") {
+                        $FinalPSObject | Add-Member -MemberType NoteProperty -Name $ippropsPropName -Value $($ipprops.$ippropsPropName)
+                    }
+                }
+                $ipUnicastPropertyNames = $($ip | Get-Member -MemberType Property).Name
+                foreach ($UnicastPropName in $ipUnicastPropertyNames) {
+                    $FinalPSObjectMemberCheck = $($FinalPSObject | Get-Member -MemberType NoteProperty).Name
+                    if ($FinalPSObjectMemberCheck -notcontains $UnicastPropName) {
+                        $FinalPSObject | Add-Member -MemberType NoteProperty -Name $UnicastPropName -Value $($ip.$UnicastPropName)
+                    }
+                }
+                $null = $PSObjectCollection.Add($FinalPSObject)
+            }
+        }
+    }
+    end {
+        return $PSObjectCollection
+    }
+}
+New-Alias -Name ifconfig -Value Get-NetworkInformation
+function Invoke-SharepointUpload {
+    [cmdletbinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Mandatory=$true)][ValidatePattern({^https://*.$})]
+        [system.uri]$AnonURL,
+        [Parameter(Mandatory=$true)]
+        [string]$Filepath
+    )
+    begin {
+        #Anonymous sharepoint links are 302 redirects, let's check
+        [system.uri]$302CheckUri = [system.uri]$AnonURL
+        do {
+            If ($302CheckUri -match "^http"){
+                $302Check =
+                #powershell core and legacy windows powershell have slightly different error ignore commands (a 302 is an error)
+                #note to test this against other version specific attributes for performance tuning later
+                switch($PSVersionTable.PSEdition){
+                    "core" {
+                        Invoke-WebRequest -Uri $302CheckUri -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue
+                    }
+                    "desktop" {
+                        Invoke-WebRequest -Uri $302CheckUri -MaximumRedirection 0 -ErrorAction SilentlyContinue
+                    }
+                }
+                switch($302Check.StatusCode){
+                    "200"   {throw "Could not find Sharepoint Library ID"}
+                    default {
+                        #If we get anything except a 200, build a hashtable of header values transformed into sharepoint uri's
+                        if ($302Check.headers.location -match "^http"){
+                            #below removes urlencode
+                            [system.uri]$redirect = $($302Check.headers.location)
+                            Do {$redirect = [System.Web.HttpUtility]::UrlDecode($redirect)}
+                            While ($redirect -match '%')
+                            #Now build uri paths
+                            $SPOUrls = [pscustomobject]@{
+                                uploadfile                 =   $(Split-Path $Filepath -Leaf)
+                                [system.uri]"site"         =   "$($redirect.Scheme)://$($redirect.Host)$($redirect.Segments[0])$($redirect.Segments[1])$($redirect.Segments[2])"
+                                [system.uri]"api"          =   "$($redirect.Scheme)://$($redirect.Host)$($redirect.Segments[0])$($redirect.Segments[1])$($redirect.Segments[2])_api"
+                                [system.uri]"apicontext"   =   "$($redirect.Scheme)://$($redirect.Host)$($redirect.Segments[0])$($redirect.Segments[1])$($redirect.Segments[2])_api/contextinfo"
+                                [system.uri]"apiweb"       =   "$($redirect.Scheme)://$($redirect.Host)$($redirect.Segments[0])$($redirect.Segments[1])$($redirect.Segments[2])_api/web"
+                                [system.uri]"sharepointid" =   if ($redirect.OriginalString -match "(id=.+)"){ $( ( ( ($redirect.OriginalString).Split("id=")[1] ).split("&")[0] ) ) }
+                            }
+                            #If we didn't get the sharepoint id need to check next hop
+                            [system.uri]$302CheckUri = $($302Check.headers.location)
+                        }
+                    }
+                }
+            }
+            Else {throw "Could not find Sharepoint Library ID"}
+        }
+        #keep doing all that stuff until we get a sharepoint id
+        until ($null -ne $SPOUrls.sharepointid)
+    }
+    process {
+        #initial web request to establish session cookiecls
+        Invoke-WebRequest -Uri $($AnonURL) -Headers @{accept = "application/json; odata=verbose" } -Method GET -SessionVariable Session
+        #Now we can make a context call to get our Request Digest
+        $ContextInfo = Invoke-RestMethod -Uri $($SPOUrls.apicontext) -Headers @{accept = "application/json; odata=verbose" } -Method Post -Body $null -ContentType "application/json;odata=verbose" -WebSession $session
+        $Digest = $ContextInfo.d.GetContextWebInformation.FormDigestValue
+        $Headers = @{
+            "Content-Type"    = "application/octet-stream"
+            "X-RequestDigest" = $Digest
+            "accept"          = "application/json;odata=verbose"
+        }
+        #Building the upload path below using the digest header
+        $UploadURI = "$($SPOURLS.apiweb)/GetFolderByServerRelativePath(DecodedUrl='$($SPOUrls.sharepointid)')/Files/Add(overwrite=true,Url='$($SPOUrls.uploadfile)')"
+    }
+    end {
+        #This is the final invoke that uploads the file to the anonymous shared library
+        Invoke-WebRequest -Method POST -Uri $UploadURI -ContentType "application/octet-stream" -Headers $headers -InFile $Filepath -WebSession $Session
+        return
+    }
+}
+function Invoke-Speedtest {
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Mandatory=$False)]
+        [switch]$Worldwide,
+        [Parameter(Mandatory=$False)]
+        [int32]$TestCount="5",
+        [Parameter(Mandatory=$False)]
+        [int32]$Size="2000"
+    )
+    begin{
+        $SpeedResults = [System.Collections.ArrayList]@()
+        $Filesize = "$($Size)x$($Size)"
+        Function downloadSpeed($strUploadUrl) {
+            #Transform the server urls needed
+            $topServerUrlSpilt = $strUploadUrl -split 'upload'
+            $url = $topServerUrlSpilt[0] + "random" + "$($Filesize)" + ".jpg"
+            #Now download some temp files and calculate speed
+            $downloadElaspedTime = (Measure-Command {$downloadfile = Invoke-WebRequest -Uri $url}).totalmilliseconds
+            $downSize = (($downloadfile).rawcontent.length) / 1Mb
+            $downloadSize = [Math]::Round($downSize, 2)
+            $downloadTimeSec = $downloadElaspedTime * 0.001
+            $downSpeed = ($downloadSize / $downloadTimeSec) * 8
+            $downloadSpeed = [Math]::Round($downSpeed, 2)
+            #Hashtable containing results
+            $SpeedResults.add(
+                [PSCustomObject]@{
+                    "Test Site"          = [string]$url.split(":8080/speedtest/")[0]
+                    "Speed(Mb)"          = [string]$downloadSpeed
+                    "Size(MiB)"          = [string]$downloadSize
+                    "Download time(sec)" = [string]$downloadTimeSec
+                    "Image Size"         = [string]$Filesize
+                }
+            )
+        }
+    }
+    process {
+        #Invoke a call to speedtest's config servers
+        $SpeedtestConfig = (Invoke-RestMethod -Uri "http://www.speedtest.net/speedtest-config.php" -SessionVariable speedtest)
+        #Speedtest will provide Latitude and Longitude of client IP
+        $ClientLat = $SpeedtestConfig.settings.client.lat
+        $ClientLon = $SpeedtestConfig.settings.client.lon
+        #Making another request to get the server list from the site.
+        $SpeedtestServers = (Invoke-RestMethod -Uri "http://www.speedtest.net/speedtest-servers.php" -WebSession $speedtest)
+        #$Serverlist can be filtered to just US servers in the speedtest.net database.
+        $Serverlist = switch ($Worldwide){
+            $True   {$SpeedtestServers.settings.servers.server }
+            $False  {$SpeedtestServers.settings.servers.server | Where-Object {$PSItem.Country -eq "United States"}}
+        }
+        #Below we calculate servers relative closeness to you by doing some math against latitude and longitude.
+        foreach($Server in $Serverlist) {
+            $R = 6371;
+            [float]$dlat = ([float]$ClientLat - [float]$Server.lat) * 3.14 / 180;
+            [float]$dlon = ([float]$ClientLon - [float]$Server.lon) * 3.14 / 180;
+            [float]$a = [math]::Sin([float]$dLat/2) * [math]::Sin([float]$dLat/2) + [math]::Cos([float]$ClientLat * 3.14 / 180 ) * [math]::Cos([float]$Server.lat * 3.14 / 180 ) * [math]::Sin([float]$dLon/2) * [math]::Sin([float]$dLon/2);
+            [float]$c = 2 * [math]::Atan2([math]::Sqrt([float]$a ), [math]::Sqrt(1 - [float]$a));
+            [float]$d = [float]$R * [float]$c;
+            $ServerInformation +=
+            @([pscustomobject]@{Distance = $d; Country = $Server.country; Sponsor = $Server.sponsor; Url = $Server.url })
+        }
+        #Now create a list of closest servers to client
+        $Localservers = ($serverinformation | Sort-Object -Property distance)[0..($TestCount)]
+        ForEach ($lserver in $Localservers) {
+            $currenturl = $lserver.url
+            downloadSpeed($currenturl) | Out-Null
+        }
+        $SpeedResults = $SpeedResults | Sort-Object -Property Speed
+    }
+    end {
+        Return $SpeedResults
+    }
+}
+function Send-NetworkInformation {
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $True, HelpMessage = "Please enter a custom name for the output file.")]
+        [ValidateNotNullOrEmpty()]
+        [String]$Filename = "$(Get-Date -Format yyyy-M-d_hh.mm.tt)_$([Environment]::MachineName).txt" ,
+        [Parameter(Position = 2, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $True, HelpMessage = "Please enter the shared upload link")]
+        [ValidateNotNullOrEmpty()]
+        [String]$AnonUrl = 'https://bit.ly/ihmnetwizupload'
+    )
+    begin {
+        $Now = Get-Date -Format yyyy-M-d_hh.mm.tt
+        $ScriptName = "Send-NetworkInformation"
+        $ScriptVersion = "1.0.2"
+        $Global:ErrorActionPreference = "Stop"
+        #Initiate Functions
+        
+        
+        #Creating logging and file paths
+        [string]$ScriptPath =
+        If ($PSScriptRoot) { $PSScriptRoot }
+        ElseIf ($PSCommandPath) { Split-Path -Parent -Path $PSCommandPath -ErrorAction SilentlyContinue }
+        ElseIf ($PWD) { $PWD }
+        ElseIf ($MyInvocation.InvocationName) { Split-Path -Parent -Path $MyInvocation.InvocationName -ErrorAction SilentlyContinue }
+        [string]$LogPath =
+        if (Test-Path "$ScriptPath\Logs") { "$ScriptPath\Logs" }
+        else { New-Item -ItemType Directory -Path "$ScriptPath\Logs" }
+        [string]$LogName = "$($now)_$($ScriptName).log"
+        [string]$LogFile = Join-Path -Path $LogPath -ChildPath $LogName
+        #Starting log as soon as we create path above
+        Start-Log -LogPath $LogPath -LogName $LogName -ScriptVersion $ScriptVersion
+        #Output file path
+        $Filepath = Join-Path -Path $ScriptPath -ChildPath $Filename
+    }
+    process{
+        try{
+            Add-LogMessage -LogPath $LogFile -Message "[NFO]    Beginning Speedtest"
+            Invoke-Speedtest | Out-File -FilePath $Filepath -Append
+            Add-LogMessage -LogPath $LogFile -Message "[NFO]    Gathering network information"
+            Get-NetworkInformation | Out-File -FilePath $Filepath -Append
+            Add-LogMessage -LogPath $LogFile -Message "[NFO]    Complete"
+        }
+        catch{
+            Add-LogError -LogPath $LogFile -LineNumber $PSItem.InvocationInfo.ScriptLineNumber -ErrorDesc "[ERR]: $($PSItem.Exception.Message)" -ExitGracefully $false
+        }
+    }
+    end{
+        try{
+            Invoke-SharepointUpload -AnonURL $AnonURL -Filepath $Filepath
+        }
+        catch{
+            Add-LogError -LogPath $LogFile -LineNumber $PSItem.InvocationInfo.ScriptLineNumber -ErrorDesc "[ERR]: $($PSItem.Exception.Message)" -ExitGracefully $false
+        }
+        finally{
+            . $filepath
+            Stop-Log -LogPath $LogFile
+        }
+    }
+}
+
+Send-NetworkInformation
 # SIG # Begin signature block
 # MIISHwYJKoZIhvcNAQcCoIISEDCCEgwCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUibpejtH8seH3g8rGFJKWoBeB
-# /t6ggg5rMIIGsDCCBJigAwIBAgIQCK1AsmDSnEyfXs2pvZOu2TANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUQGpG4n9EAN56zcIr4fxEo7Bd
+# 2PSggg5rMIIGsDCCBJigAwIBAgIQCK1AsmDSnEyfXs2pvZOu2TANBgkqhkiG9w0B
 # AQwFADBiMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
 # VQQLExB3d3cuZGlnaWNlcnQuY29tMSEwHwYDVQQDExhEaWdpQ2VydCBUcnVzdGVk
 # IFJvb3QgRzQwHhcNMjEwNDI5MDAwMDAwWhcNMzYwNDI4MjM1OTU5WjBpMQswCQYD
@@ -415,16 +691,16 @@ Function Send-Log {
 # ZyBSU0E0MDk2IFNIQTM4NCAyMDIxIENBMQIQC4uUnYEEkqZXl45C8AoenDAJBgUr
 # DgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMx
 # DAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkq
-# hkiG9w0BCQQxFgQUAhFb1t7mmWFyBk0/EiSkzpw0JqcwDQYJKoZIhvcNAQEBBQAE
-# ggIAGS1uKkRLiLKVKgqhHtGg4P4JOvJgQ9C6IviZqx5M67+IhdDNkwd2bDLuLFTP
-# ygZ7tLx0WKOfgDW5s+LeUDcuiLRwB1XJiRRT/cTVMLmGgCE+1eXUW7r7fG5PwGAp
-# dXCtmrYQGGlpLHcPvA43IJWmBrKHlUwM9coVgOQ8zSl04l04oQdoz6myuCpLBSqL
-# J2Sulv7wMK/EH7u9Mcfq9m0EqiGFu79rFgNOrcm07Rrv3YECchJoy9792vD9N6/S
-# 0RjG5+sE+mTg6x8GCUUfrf+3pAXUIhgh00j9ePuLW8hoZraXF6z1AirHCDLZrNYF
-# uQkg3V55G+b5DUh3HZelX+V+f9liXc+y7abH1a2gl9LbBizmn+vj5uAD02pYcYYb
-# nr1Pu0FQV6FZU9Ii5LC6VNfTxnEH7CgEyD9t4pa9cdOnYGwJ5xvrUzhfzja50y2d
-# WUoeQ7hNRZLYbr0ZHX8n2tBscDPO03u3NuRsAqE1AzFj+/i2IGQnSa7XY9kTLQVz
-# pfYrMWYQRZ55TwTRH9p+LILCmTxLSBChywDfn6QCcrZIg+Q67lam0mJeoYcUFcjL
-# wGRUF2cCFJMszNb+bHK/b5RH5rm/PnQW8nz0GJrmF8FzO2QIfTW90j9bbRW0984I
-# wXUid8KXVTr2h01eRoJ/cX1DyuUmOT5yyzEQ9Q57M178IVU=
+# hkiG9w0BCQQxFgQU2ArQNWr9nVq/E3ICVSBGNuLYFLswDQYJKoZIhvcNAQEBBQAE
+# ggIASuzGses6A1VRH8l7uJ/+uEOeANc3pKDKIwNFN95uhg2ZbJyjiGy7nWq+oV4+
+# PfISs8YTg/YJxgAHk2W3rrKP3MvapILjdPCjxS/2WrQrLpUBvm6c1j6IgmX+AY8X
+# p0+439NtfbXrs2O1zs+/9P7gkiqFzJjVi92f4ww3soZ1MV4T8JfnyFcmFmlHIw9z
+# m5lNvAEEtXVfYTFqGpMMV7xeMqkIGFwVbjsNoBFQ4qjRh2N1IDcxQwfJ3LdnzokX
+# Ok9DHUPiZVsfg8IidmaFWrssice28/vCEMS2j/6rM10GMQbQuTqQzUq1O2t5cpC2
+# T0ZutnKRCWvlcGb986ys9/+Gs8hu4QKEQcGA0JDY24SRZXzrThXebRq7J99nGiQ3
+# 3Ih9nzuVNnGqt9iKrzwJm9gYnT5uEoSqrhSIUJGRpY8jlXrSijvS4neTdZpWHE5v
+# 5wsDktybDZZ9HP0M75OyTia/0RrPDnNtcZyzby85jOWCmfbEF94oIvp5urWAmug0
+# F5s7QgSD4fgZcTJIlcuDvf4Rw+A36G8saLfixHSLZqJ2ANpnTq86MU+tSSJnVdNQ
+# fF+wu+fYMXi5bAp/ce4pNQbB3Z/wr2PysB602auxn0f0RkITLyj+x8BHm+x6NUyq
+# Ozxxr9m6cxpUvtPAZZ/rKRknIWM64nlPTVDSJ9bchqbiCfM=
 # SIG # End signature block
